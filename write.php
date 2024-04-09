21d9f319901d6c2dffa1ae7e5b770f4dd367abb7
Soyalguien2324 <131987116+Soyalguien2324@users.noreply.github.com>
2024-04-09 20:04:01 +0200

    Update write.php

--- write.php
+++ write.php
@@ -1,5 +1,5 @@
 <?php
-	$archivo = "/var/www/html/chat.txt";
+	$archivo = "./chat.txt";
 	$fp = fopen($archivo, "a+"); 
 	$text = "<br>" . "[" . $_GET['user'] . "] " . $_GET['msg'] . "\n". "<br />";
 	if ($fp) {
